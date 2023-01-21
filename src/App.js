6d55d65ae732af1ea3af3e7ceece9fd6f3790fb5
saidamakhchand <saidamakh21@gmail.com>
2023-01-21 18:06:39 +0100

    react todo

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Cc from './components/Cc';
+
+
+function App() {
+  return (
+
+    <div className="App">
+     
+      <Cc/>
+
+    </div>
+  );
+}
+export default App;
